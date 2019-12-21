4554a89f9fc1df11b83fe9dfd77fcb123c26f000
KisChang <734615869@qq.com>
2019-12-21 17:29:56 +0800

    修改启动脚本

--- mydata/home/tc/start-run.sh
+++ mydata/home/tc/start-run.sh
@@ -3,4 +3,4 @@
 export JAVA_HOME=/home/tc/jre
 export PATH=$JAVA_HOME/bin:$PATH
 
-sudo jre/bin/java -jar /home/tc/jre/omshard.jar /home/tc/program.conf
+sudo /home/tc/jre/bin/java -jar /home/tc/omshard.jar
